6625e72986558ae4f76780d16cd59314450e5a7e
Gilmara <gilmaralopes2006@gmail.com>
2023-04-23 22:29:53 -0300

    [Feat]:rocket: Retorne o nome do produto, a quantidade de curtidas e quantos itens foram vendidos dos produtos que não sejam iguais a Big Mac e McChicken

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,4 +1,6 @@
-db.produtos.find({ valoresNutricionais: {
-  $elemMatch: { tipo: "proteínas", percentual: { $lte: 40, $gte: 30 } },
-} },
-{ nome: 1, _id: 0 });
+db.produtos.find({
+  valoresNutricionais: {
+    $elemMatch: { tipo: "proteínas", percentual: { $lte: 40, $gte: 30 } },
+  }
+},
+  { nome: 1, _id: 0 });
